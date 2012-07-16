d750e878c67fe560a7e433ede340f87f7229756e
noamkfir <noam@kfir.cc>
2012-07-16 22:12:33 +0300

    Temporary removed "disclosure" icon from agenda list items.

--- src/web/www/javascripts/views/AgendaListView.js
+++ src/web/www/javascripts/views/AgendaListView.js
@@ -5,7 +5,7 @@ OKnesset.app.views.AgendaListView = new Ext.extend(Ext.List, {
     id: 'AgendaListView',
     store: OKnesset.AgendaStore,
     itemTpl: '<div class="agendaName">{name}<div class="agendaVotes">{number_of_items} ' + OKnesset.strings.votes + '</div></div>',
-    onItemDisclosure: true
+    onItemDisclosure: false
 });
 
 Ext.reg('AgendaListView', OKnesset.app.views.AgendaListView);
